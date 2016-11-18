f9b05980ab249ecb10ca2fd7766e9040812b2907
Matthew Spivey <spivey.matthew@gmail.com>
2016-11-18 17:46:59 -0500

    Export fix

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,28 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _actionCreators = require('./actionCreators');
-
-var _actionCreators2 = _interopRequireDefault(_actionCreators);
+exports.reduce = exports.isActionValid = exports.actionCreators = exports.PSEUDOSTATES = exports.STATES = exports.ACTIONS = undefined;
 
 var _constants = require('./constants');
+
+Object.defineProperty(exports, 'ACTIONS', {
+  enumerable: true,
+  get: function get() {
+    return _constants.ACTIONS;
+  }
+});
+Object.defineProperty(exports, 'STATES', {
+  enumerable: true,
+  get: function get() {
+    return _constants.STATES;
+  }
+});
+Object.defineProperty(exports, 'PSEUDOSTATES', {
+  enumerable: true,
+  get: function get() {
+    return _constants.PSEUDOSTATES;
+  }
+});
 
 var _MatchConfig = require('./MatchConfig');
 
@@ -18,16 +34,16 @@ var _StateMachine = require('./StateMachine');
 
 var _StateMachine2 = _interopRequireDefault(_StateMachine);
 
+var _actionCreators = require('./actionCreators');
+
+var _actionCreators2 = _interopRequireDefault(_actionCreators);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var matchConfig = new _MatchConfig2.default();
-var stateMachine = new _StateMachine2.default(matchConfig);
+exports.actionCreators = _actionCreators2.default;
 
-exports.default = {
-  actionCreators: _actionCreators2.default,
-  isActionValid: stateMachine.isActionValid,
-  reduce: stateMachine.reduce,
-  ACTIONS: _constants.ACTIONS,
-  STATES: _constants.STATES,
-  PSEUDOSTATES: _constants.PSEUDOSTATES
-};
+
+var stateMachine = new _StateMachine2.default(new _MatchConfig2.default());
+
+var isActionValid = exports.isActionValid = stateMachine.isActionValid;
+var reduce = exports.reduce = stateMachine.reduce;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
-import actionCreators from './actionCreators';
-import {ACTIONS, STATES, PSEUDOSTATES} from './constants';
+
 import MatchConfig from './MatchConfig';
 import StateMachine from './StateMachine';
+import actionCreators from './actionCreators';
 
-const matchConfig = new MatchConfig();
-const stateMachine = new StateMachine(matchConfig);
+export {ACTIONS, STATES, PSEUDOSTATES} from './constants';
+export {actionCreators};
 
-export default {
-  actionCreators,
-  isActionValid: stateMachine.isActionValid,
-  reduce: stateMachine.reduce,
-  ACTIONS,
-  STATES,
-  PSEUDOSTATES
-};
+const stateMachine = new StateMachine(new MatchConfig());
+
+export let isActionValid = stateMachine.isActionValid;
+export let reduce = stateMachine.reduce;
