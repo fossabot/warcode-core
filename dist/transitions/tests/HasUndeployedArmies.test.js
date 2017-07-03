7b8f672507af5fa89a682c141d24d0f4a4795084
Matthew Spivey <spivey.matthew@gmail.com>
2017-07-02 21:21:46 -0400

    Fixing documentation

---
+++ dist/transitions/tests/HasUndeployedArmies.test.js
@@ -0,0 +1,72 @@
+'use strict';
+
+var _expect = require('expect');
+
+var _expect2 = _interopRequireDefault(_expect);
+
+var _HasUndeployedArmies = require('../HasUndeployedArmies');
+
+var _HasUndeployedArmies2 = _interopRequireDefault(_HasUndeployedArmies);
+
+var _MatchConfig = require('../../MatchConfig');
+
+var _MatchConfig2 = _interopRequireDefault(_MatchConfig);
+
+var _actionCreators = require('../../actionCreators');
+
+var _actionCreators2 = _interopRequireDefault(_actionCreators);
+
+var _config = require('./config.json');
+
+var _config2 = _interopRequireDefault(_config);
+
+var _constants = require('../../constants');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var matchConfig = (0, _MatchConfig2.default)(_config2.default);
+var matchExtendedState = {
+  stateKey: _constants.STATES.BATTLING,
+  currentPlayerIndex: 0,
+  territories: [{
+    owner: 1,
+    armies: 3
+  }, {
+    owner: 0,
+    armies: 6
+  }, {
+    owner: 1,
+    armies: 3
+  }],
+  players: [{
+    undeployedArmies: 0
+  }, {
+    undeployedArmies: 0
+  }]
+};
+// TODO - try handling of incorrect action with deployed armies
+// TODO - try 0 undeployed armiesS
+// TODO - try correct type and stateS
+
+test('guard checks state', function () {
+  var transition = (0, _HasUndeployedArmies2.default)(matchConfig, matchExtendedState);
+  var action = _actionCreators2.default.startMatch(5);
+  (0, _expect2.default)(transition.guard(action)).toEqual(false);
+});
+
+test('guard checks that player has deployed all their armies', function () {
+  var transition = (0, _HasUndeployedArmies2.default)(matchConfig, matchExtendedState);
+  var action = _actionCreators2.default.placeNewArmies(0, 1);
+  (0, _expect2.default)(transition.guard(action)).toEqual(false);
+});
+
+test('guard is true when player has undeployed armies', function () {
+  var matchExtendedStateCopy = Object.assign({}, matchExtendedState, {
+    players: [{ undeployedArmies: 1 }, { undeployedArmies: 0 }]
+  });
+  var transition = (0, _HasUndeployedArmies2.default)(matchConfig, matchExtendedStateCopy);
+  var action = _actionCreators2.default.placeNewArmies(0, 1);
+  (0, _expect2.default)(transition.guard(action)).toEqual(true);
+  var state = Object.assign({}, matchExtendedStateCopy, transition.reduce(action));
+  (0, _expect2.default)(state).toEqual(matchExtendedStateCopy);
+});
