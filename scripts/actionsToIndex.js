f0c7bdc29bd3ae02b68f1ef10c113869a70d1d53
Matthew Spivey <spivey.matthew@gmail.com>
2017-07-04 21:08:50 -0400

    Adding getting started instructions

--- scripts/actionsToIndex.js
+++ scripts/actionsToIndex.js
@@ -1,6 +1,39 @@
 const { ACTIONS, STATES, PSEUDOSTATES } = require('../src/constants');
 const fs = require('fs');
 
+const gettingStared = () => `## Getting Started
+
+### Install the package
+\`\`\` shell
+npm init
+npm install --save matthewspivey/warcode-core#gh-pages
+\`\`\`
+
+### index.js
+\`\`\` javascript
+const { actionCreators, reduce } = require('warcode-core');
+
+let state = reduce();
+console.log(state.stateKey);
+state = reduce(state, actionCreators.startMatch(3));
+console.log(state.stateKey);
+state = reduce(state, actionCreators.selectFirstPlayer(0));
+console.log(state.stateKey);
+\`\`\`
+
+### Run the filew
+\`\`\` shell
+node index.js
+\`\`\`
+
+### Expected output
+\`\`\`
+Initializing
+SelectingFirstPlayer
+OccupyingTerritorygettingStared
+\`\`\`
+`;
+
 const toRow = ({ name, markdownURL, doc }) => {
   const summary = !doc.summary ? '' : doc.summary.children[0].children[0].value.replace(/(\r\n|\n|\r\w)/gm, ' ').replace(/\s\s+/g, ' ');
 
@@ -8,6 +41,8 @@ const toRow = ({ name, markdownURL, doc }) => {
 };
 
 module.exports = (actions, stateDiagramURL) => `
+${gettingStared()}
+
 ## Gameplay State machine
 
 Gameplay includes many phases. These are illustrated in the following state diagram.
