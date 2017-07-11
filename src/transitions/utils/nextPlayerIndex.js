086bba54948bdd5da1a2c34c3bed737bcb7eba6a
Matthew Spivey <spivey.matthew@gmail.com>
2017-07-10 22:10:57 -0400

    More λ

--- src/transitions/utils/nextPlayerIndex.js
+++ src/transitions/utils/nextPlayerIndex.js
@@ -1,31 +1,17 @@
 // @flow
 import type { MatchState } from '../../MatchState';
 
-const activePlayerIndicies = ({ playersUndeployedArmies, territories }) => {
-  const indicies = new Set();
-
-  // TODO - replace with filter().map() to avoid mutation
-  // set players with undeployed armies
-  if (Array.isArray(playersUndeployedArmies)) {
-    playersUndeployedArmies.forEach((armies, i) => {
-      if (armies >= 1) {
-        indicies.add(i);
-      }
-    });
-  }
-
-  // set players occupying territories
-  if (Array.isArray(territories)) {
-    territories.forEach(territory => {
-      if (Number.isInteger(territory.owner)) {
-        indicies.add(territory.owner);
-      }
-    });
-  }
-
+const activePlayerIndicies = ({ playersUndeployedArmies, territories }) =>
   // eslint-disable-next-line fp/no-mutating-methods
-  return Array.from(indicies).sort();
-};
+  Array.from(
+    new Set([
+      ...playersUndeployedArmies
+        .map((armies, i) => ({ armies, i }))
+        .filter(u => u.armies > 0)
+        .map(u => u.i),
+      ...territories.filter(t => Number.isInteger(t.owner)).map(t => t.owner),
+    ])
+  ).sort();
 
 // MUST BE RUN AT END OF TURN, USING EXTENDED STATE TO BE RETURNED BY REDUCER
 export default function nextPlayerIndex({
@@ -34,11 +20,6 @@ export default function nextPlayerIndex({
   currentPlayerIndex,
 }: MatchState) {
   const activePlayers = activePlayerIndicies({ playersUndeployedArmies, territories });
-
   const i = activePlayers.indexOf(currentPlayerIndex);
-  if (i + 1 < activePlayers.length) {
-    return activePlayers[i + 1];
-  }
-
-  return activePlayers[0];
+  return activePlayers[i + 1 < activePlayers.length ? i + 1 : 0];
 }
